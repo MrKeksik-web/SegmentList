cae553e0f71a3acb6d4ac636b1e91e3b2bb6b0ce
Aivar Minsafin <min.aivar2001@gmail.com>
2020-03-29 18:41:30 +0300

    SegmentList was fixed #2

--- segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/SegmentList.java
+++ segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/SegmentList.java
@@ -56,8 +56,6 @@ public class SegmentList {
         for (int i = 0; i < segments.size(); i++) {
             for (int j = 0; j < segments.size(); j++) {
                 if (segments.get(i).getLength() < segments.get(j).getLength()) {
-
-
                     Segment seg = segments.get(i);
                     segments.set(i, segments.get(j));
                     segments.set(j, seg);
