67d1c686d0cd5fa5ee15b746c3386a92bfba2a42
Dmitry <diman2190@gmail.com>
2020-03-29 16:32:02 +0300

    all methods are implemented

--- segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/SegmentList.java
+++ segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/SegmentList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class SegmentList {
     protected ArrayList<Segment> segments;
+    private int size;
 
     public SegmentList(String path) {
 
@@ -19,8 +20,34 @@ public class SegmentList {
         }
     }
 
-    public void test(){
+    public SegmentList angleList(){
+        segments.stream()
+            .filter(segment -> {
+                double k = (segment.getSecondYCoordinate() - segment.getFirstYCoordinate())/(segment.getSecondXCoordinate() - segment.getFirstXCoordinate());
+                if(Math.sqrt(3)/3 == k || 1 == k){
+                    return true;
+                }
+                return false;
+            });
+        return this;
+    }
 
+    public SegmentList lengthList(int a,int b){
+        segments.stream()
+                .filter(segment -> segment.getLength() >= a && segment.getLength() <= b);
+        return this;
+    }
+
+    public void sort(){
+        for(int i = 0;i < size;i++){
+            for(int j = 0;j < size;j++){
+                if(segments.get(i).getLength() < segments.get(j).getLength()){
+                    Segment seg = segments.get(i);
+                    segments.set(i,segments.get(j));
+                    segments.set(j,seg);
+                }
+            }
+        }
     }
 
 }
