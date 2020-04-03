064bda1cfd4de09c0ca40339d2fd283f981af9eb
Aivar Minsafin <min.aivar2001@gmail.com>
2020-04-03 10:26:57 +0300

    MethodTester was added (1, 3, 5 methods) + test

--- segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/Main.java
+++ segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/Main.java
@@ -15,16 +15,27 @@ public class Main {
         test();
     }
 
-    public static void generate(){
+    public static void generate() {
         SegmentTestsGenerator generator = new SegmentTestsGenerator(50);
         SegmentList list = new SegmentList("output.txt");
         list.show();
     }
 
-    public static void test(){
+    public static void test() {
         MethodTester tester = new MethodTester();
-        System.out.println(tester.constructorTime()); //1
-        System.out.println(tester.insertTime());//3
-        System.out.println(tester.lengthListTime());//5
+        double average1 = 0;
+        double average2 = 0;
+        double average3 = 0;
+        for (int i = 0; i < 100; i++) {
+            average1 += tester.constructorTime();//1
+            average2 += tester.insertTime();//3
+            average3 += tester.lengthListTime();//5
+        }
+        average1 = average1/100.0;
+        average2 = average2/100.0;
+        average3 = average3/100.0;
+        System.out.println(average1); //1
+        System.out.println(average2);//3
+        System.out.println(average3);//5
     }
 }
