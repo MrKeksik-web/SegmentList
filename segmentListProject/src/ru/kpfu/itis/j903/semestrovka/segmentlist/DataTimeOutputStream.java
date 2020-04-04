5d05aedfe7ca3ebb2d384cb3b4d1df7bd74e5dff
Dmitry <diman2190@gmail.com>
2020-04-04 15:22:43 +0300

    Added time record to a file, written DataTimeOutputStream class

---
+++ segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/DataTimeOutputStream.java
@@ -0,0 +1,77 @@
+package ru.kpfu.itis.j903.semestrovka.segmentlist;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class DataTimeOutputStream extends OutputStream {
+    private DataOutputStream out;
+
+    public DataTimeOutputStream(OutputStream out) {
+        this.out = new DataOutputStream(out);
+    }
+
+    public void writeTime(ArrayList arr) throws IOException {
+        out.writeChars(String.valueOf("N\t"));
+        out.writeChars(String.valueOf("Time\n"));
+        for(int i = 0;i < arr.size();i++){
+            out.writeChars(String.valueOf(i + "\t"));
+            out.writeChars(String.valueOf(arr.get(i) + "\n"));
+        }
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        out.write(b);
+    }
+
+    public void writeBoolean(boolean v) throws IOException {
+        out.writeBoolean(v);
+    }
+
+    public void writeByte(int v) throws IOException {
+        out.writeByte(v);
+    }
+
+    public void writeShort(int v) throws IOException {
+        out.writeShort(v);
+    }
+
+    public void writeChar(int v) throws IOException {
+        out.writeChar(v);
+    }
+
+    public void writeInt(int v) throws IOException {
+        out.writeInt(v);
+    }
+
+    public void writeLong(long v) throws IOException {
+        out.writeLong(v);
+    }
+
+    public void writeFloat(float v) throws IOException {
+        out.writeFloat(v);
+    }
+
+    public void writeDouble(double v) throws IOException {
+        out.writeDouble(v);
+    }
+
+    public void writeBytes(String s) throws IOException {
+        out.writeBytes(s);
+    }
+
+    public void writeChars(String s) throws IOException {
+        out.writeChars(s);
+    }
+
+    public void writeUTF(String str) throws IOException {
+        out.writeUTF(str);
+    }
+
+    public int size() {
+        return out.size();
+    }
+}
--- segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/Main.java
+++ segmentListProject/src/ru/kpfu/itis/j903/semestrovka/segmentlist/Main.java
@@ -3,11 +3,9 @@ package ru.kpfu.itis.j903.semestrovka.segmentlist;
 
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -15,39 +13,93 @@ public class Main {
         test();
     }
 
-    public static void generate() {
-        SegmentTestsGenerator generator = new SegmentTestsGenerator(50);
+    public static void generate(int count) {
+        SegmentTestsGenerator generator = new SegmentTestsGenerator(count);
         SegmentList list = new SegmentList("output.txt");
         list.show();
     }
 
-    public static void test() {
-        MethodTester tester = new MethodTester();
-        double average1 = 0;
-        double average2 = 0;
-        double average3 = 0;
-        double average4 = 0;
-        double average5 = 0;
-        double average6 = 0;
-        for (int i = 0; i < 100; i++) {
-            average1 += tester.constructorTime();//1
-            average2 += tester.showTime();//2
-            average3 += tester.insertTime();//3
-            average4 += tester.angleTime();//4
-            average5 += tester.lengthListTime();//5
-            average6 += tester.sortTime();//6
+    public static void writeTime(ArrayList arr,String path){
+        try(DataTimeOutputStream out = new DataTimeOutputStream(new FileOutputStream(path))) {
+            out.writeTime(arr);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        average1 = average1/100.0;
-        average2 = average2/100.0;
-        average3 = average3/100.0;
-        average4 = average4/100.0;
-        average5 = average5/100.0;
-        average6 = average6/100.0;
-        System.out.println(average1); //1
-        System.out.println(average2);//2
-        System.out.println(average3);//3
-        System.out.println(average4);//4
-        System.out.println(average5);//5
-        System.out.println(average6);//6
+    }
+
+    public static void test(){
+        MethodTester tester = new MethodTester();
+        ArrayList<double[]> arrTimes = new ArrayList();
+        int testsCount = 5;
+        for(int i = 0;i < testsCount;i++){
+            double[] arrAverages = new double[6];
+            generate(i);
+            for(int j = 0;j < 1000;j++){
+                arrAverages[0] += tester.constructorTime();
+                arrAverages[1] += tester.showTime();
+                arrAverages[2] += tester.insertTime();
+                arrAverages[3] += tester.angleTime();
+                arrAverages[4] += tester.lengthListTime();
+                arrAverages[5] += tester.sortTime();
+            }
+            arrAverages[0] = arrAverages[0]/1000;
+            arrAverages[1] = arrAverages[1]/1000;
+            arrAverages[2] = arrAverages[2]/1000;
+            arrAverages[3] = arrAverages[3]/1000;
+            arrAverages[4] = arrAverages[4]/1000;
+            arrAverages[5] = arrAverages[5]/1000;
+            arrTimes.add(arrAverages);
+        }
+
+        ArrayList arrConst = new ArrayList();
+        ArrayList arrShow = new ArrayList();
+        ArrayList arrIns = new ArrayList();
+        ArrayList arrAngle = new ArrayList();
+        ArrayList arrLength = new ArrayList();
+        ArrayList arrSort = new ArrayList();
+
+        for(int i = 0;i < testsCount;i++){
+            double[] arr2 = arrTimes.get(i);
+            arrConst.add(arr2[0]);
+            arrShow.add(arr2[1]);
+            arrIns.add(arr2[2]);
+            arrAngle.add(arr2[3]);
+            arrLength.add(arr2[4]);
+            arrSort.add(arr2[5]);
+        }
+        writeTime(arrConst,"constTimes.txt");
+        writeTime(arrShow,"showTimes.txt");
+        writeTime(arrIns,"insTimes.txt");
+        writeTime(arrAngle,"angleTimes.txt");
+        writeTime(arrLength,"lengthTimes.txt");
+        writeTime(arrSort,"sortTimes.txt");
+//        MethodTester tester = new MethodTester();
+//        generate();
+//        double average1 = 0;
+//        double average2 = 0;
+//        double average3 = 0;
+//        double average4 = 0;
+//        double average5 = 0;
+//        double average6 = 0;
+//        for (int i = 0; i < 1000; i++) {
+//            average1 += tester.constructorTime();//1
+//            average2 += tester.showTime();//2
+//            average3 += tester.insertTime();//3
+//            average4 += tester.angleTime();//4
+//            average5 += tester.lengthListTime();//5
+//            average6 += tester.sortTime();//6
+//        }
+//        average1 = average1/100.0;
+//        average2 = average2/1000.0;
+//        average3 = average3/100.0;
+//        average4 = average4/100.0;
+//        average5 = average5/100.0;
+//        average6 = average6/100.0;
+//        System.out.println(average1); //1
+//        System.out.println(average2);//2
+//        System.out.println(average3);//3
+//        System.out.println(average4);//4
+//        System.out.println(average5);//5
+//        System.out.println(average6);//6
     }
 }
